e8e6320c7a20490af7b7ef7df3fcf5d51800c97f
Aathira <wpepele@gmail.com>
2023-08-06 13:34:04 +0200

    refactoring

--- ShopOnlineSolution/ShopOnline.Web/Pages/ProductsBase.cs
+++ ShopOnlineSolution/ShopOnline.Web/Pages/ProductsBase.cs
@@ -13,5 +13,16 @@ namespace ShopOnline.Web.Pages
         {
             Products = await ProductService.GetItems();
         }
+        protected IOrderedEnumerable<IGrouping<int,ProductDto>> GetGroupedProductsByCategory()
+        {
+            return from product in Products
+                   group product by product.CategoryId into prodByCatGroup
+                   orderby prodByCatGroup.Key
+                   select prodByCatGroup;
+        }
+        protected string GetCateogryName(IGrouping<int, ProductDto> groupedProductDtos)
+        {
+            return groupedProductDtos.FirstOrDefault(pg => pg.CategoryId == groupedProductDtos.Key).CategoryName;
+        }
     }
 }
